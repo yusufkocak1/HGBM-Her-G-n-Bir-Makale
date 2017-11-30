8cc1cb5b93d5c96c2b2aa6939d948b91779326ed
yusufkocak1 <yusufkocak.53@yandex.com>
2017-11-30 22:12:49 +0300

    fragment

---
+++ app/src/main/java/yube/com/hgbmhergnbirmakale/activity/MainActivity.java
@@ -0,0 +1,30 @@
+package yube.com.hgbmhergnbirmakale.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+
+import yube.com.hgbmhergnbirmakale.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private Button btnSimpleTabs, btnScrollableTabs, btnIconTextTabs, btnIconTabs, btnCustomIconTextTabs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(MainActivity.this, SimpleTabsActivity.class));
+
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/yube/com/hgbmhergnbirmakale/activity/SimpleTabsActivity.java
@@ -0,0 +1,126 @@
+package yube.com.hgbmhergnbirmakale.activity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import yube.com.hgbmhergnbirmakale.R;
+import yube.com.hgbmhergnbirmakale.fragments.OneFragment;
+import yube.com.hgbmhergnbirmakale.fragments.ThreeFragment;
+import yube.com.hgbmhergnbirmakale.fragments.TwoFragment;
+
+
+public class SimpleTabsActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_tabs);
+
+        new text().execute();
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "Bilim");
+        adapter.addFragment(new TwoFragment(), "Kültür");
+        adapter.addFragment(new ThreeFragment(), "Edebiyat");
+        viewPager.setAdapter(adapter);
+    }
+
+
+    private class text extends AsyncTask<Void, Void, Void> {
+        String text;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Document doc = Jsoup.connect("http://themusicplayer.org/html/hgbm.php").get();
+                Elements info = doc.select("body");
+
+                text=info.text();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            // Set description into TextView
+            TextView txtdesc = (TextView) findViewById(R.id.text);
+            txtdesc.setText(text);
+
+        }
+
+
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
